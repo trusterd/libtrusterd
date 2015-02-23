e6308fbacb9bf6773c94d3cd8d6da08f3a4b7c3c
Junichi Kajiwara <junichi.kajiwara@gmail.com>
2015-02-24 06:59:52 +0900

    oops! update trusterdGo.go

--- examples/trusterdGo.go
+++ examples/trusterdGo.go
@@ -11,23 +11,32 @@ package main
 
  int boot(char*, FUNCPTR);
 
- static void testGoGo() {
- boot("puts 1+2",(FUNCPTR)myCallback);
+ static void testGoGo(char *scpt) {
+ //boot("puts 1+2",(FUNCPTR)myCallback);
+ boot(scpt,(FUNCPTR)myCallback);
  printf("%s","hello, Go!\n");
  fflush(stdout);
  }
 */
 import "C"
 
-import . "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+)
 
 //export myCallback
 func myCallback(s *C.char) C.int {
-    //C.printf("%s\n",s)
-    Println(C.GoString(s))
-    return 0
+	//C.printf("%s\n",s)
+	fmt.Println(C.GoString(s))
+	return 0
 }
 
 func main() {
-    C.testGoGo()
+	rbscript, err := ioutil.ReadFile("../trusterd.conf.rb")
+	if err != nil {
+		fmt.Println(rbscript, err)
+		return
+	}
+	C.testGoGo(C.CString(string(rbscript)))
 }
