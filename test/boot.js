0b65282e6a708f03b711f632338f5b3622d304e4
Junichi Kajiwara <junichi.kajiwara@gmail.com>
2015-02-25 15:38:02 +0900

    Update boot.js

--- test/boot.js
+++ test/boot.js
@@ -5,42 +5,44 @@ var ffi = require('ffi');
 describe('libtrusterd boot test', function () {
   afterEach(gc)
 
-it('should be work :)', function () {
-var funcPtr = ffi.Function('int', ['string']);
-var mylib = ffi.Library('./libtrusterd', {
-'boot': ['int', ['string', funcPtr]]
-});
-var script = "puts 1+2";
+  it('should be work :)', function () {
+    var funcPtr = ffi.Function('int', ['string']);
+    var mylib = ffi.Library('./libtrusterd', {
+      'boot': ['int', ['string', funcPtr]]
+    });
+    var script = "puts 1+2";
 
-// onReult will call by trusterd.
-var onResult = function(resultVal) {
-  console.log('Result is', resultVal);
-  //assert.equal(script.length,);
-  return 0;
-}
-// start http2 trusterd.
+    // onReult will call by trusterd.
+    var onResult = function(resultVal) {
+      assert(false) // shouldn't get here
+      console.log('Result is', resultVal);
+      
+      //assert.equal(script.length,);
+      return 0;
+    }
+    
+    // start mruby
+    assert.equal(script.length,mylib.boot(script,onResult));
+    
+  });
 
-assert.equal(script.length,mylib.boot(script,onResult));
-});
+  it('should be callback from mruby:)', function () {
+    var testString = "abc0123";
+    var funcPtr = ffi.Function('int', ['string']);
+    var mylib = ffi.Library('./libtrusterd', {
+      'boot': ['int', ['string', funcPtr]]
+    });
+    var script = 'MyCall.my_exec('+testString+')';
 
-it('should be callback from mruby:)', function () {
-  var testString = "abc0123";
-var funcPtr = ffi.Function('int', ['string']);
-var mylib = ffi.Library('./libtrusterd', {
-'boot': ['int', ['string', funcPtr]]
-});
-var script = 'MyCall.my_exec('+testString+')';
+    // onReult will call by trusterd.
+    var onResult = function(resultVal) {
+      console.log('Result is', resultVal);
+      assert.equal("",resultVal);
+      return 0;
+    }
+    // start http2 trusterd.
 
-// onReult will call by trusterd.
-var onResult = function(resultVal) {
-  console.log('Result is', resultVal);
-  assert.equal("",resultVal);
-  return 0;
-}
-// start http2 trusterd.
-
-//assert.equal(script.length,mylib.boot(script,onResult));
-});
-
+    //assert.equal(script.length,mylib.boot(script,onResult));
+  });
 });
 
