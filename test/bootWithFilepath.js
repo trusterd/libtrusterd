fb785e54361b64df08f0289463510020534ae17b
Junichi Kajiwara <junichi.kajiwara@gmail.com>
2015-02-27 06:43:02 +0900

    correct test code

--- test/bootWithFilepath.js
+++ test/bootWithFilepath.js
@@ -3,24 +3,22 @@ var ref = require('ref');
 var ffi = require('ffi');
 
 describe('libtrusterd boot_with_filepath test', function() {
-    afterEach(gc)
+  afterEach(gc)
 
-    it('should be work :)', function() {
-      var funcPtr = ffi.Function('int', ['string']);
-      var mylib = ffi.Library('./libtrusterd', {
-        'boot_with_filepath': ['int', ['string', funcPtr]]
-      });
-      var filepath = "./trusterd.test01.conf.rb";
-
-      // onReult will call by trusterd.
-      var onResult = function(resultVal) {
-        assert(false) // shouldn't get here
-        return 0;
-      }
-
-      // start mruby
-      assert.equal(script.length, mylib.boot(filepath, onResult));
-
+  it('should be work :)', function() {
+    var funcPtr = ffi.Function('int', ['string']);
+    var mylib = ffi.Library('./libtrusterd', {
+      'boot_with_filepath': ['int', ['string', funcPtr]]
     });
+    var filepath = "./trusterd.test01.conf.rb";
+
+    // onReult will call by trusterd.
+    var onResult = function(resultVal) {
+      assert(false) // shouldn't get here
+      return 0;
+    }
+
+    // start mruby
+    assert.equal(script.length, mylib.boot(filepath, onResult));
   });
-})
+});
