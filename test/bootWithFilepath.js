f5ccf1cfe121f2119af9ec6e6ba2044f9b2de899
Junichi Kajiwara <junichi.kajiwara@gmail.com>
2015-02-27 05:54:19 +0900

    add new api test

---
+++ test/bootWithFilepath.js
@@ -0,0 +1,24 @@
+var assert = require('assert');
+var ref = require('ref');
+var ffi = require('ffi');
+
+describe('libtrusterd boot_with_filepath test', function () {
+  afterEach(gc)
+
+  it('should be work :)', function () {
+    var funcPtr = ffi.Function('int', ['string']);
+    var mylib = ffi.Library('./libtrusterd', {
+      'boot_with_filepath': ['int', ['string', funcPtr]]
+    });
+    var script = "puts 1+2";
+
+    // onReult will call by trusterd.
+    var onResult = function(resultVal) {
+      assert(false) // shouldn't get here
+      return 0;
+    }
+
+    // start mruby
+    assert.equal(script.length,mylib.boot(script,onResult));
+
+  });
