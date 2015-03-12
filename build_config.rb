426eee16549b087c1edef677acbe2edfc0d240a9
Junichi Kajiwara <junichi.kajiwara@gmail.com>
2015-03-13 06:40:05 +0900

    darwin needs siginal

--- build_config.rb
+++ build_config.rb
@@ -14,10 +14,10 @@ MRuby::Build.new do |conf|
   conf.gem :github => 'iij/mruby-process'
   conf.gem :github => 'mattn/mruby-onig-regexp'
   conf.gem :github => 'mattn/mruby-json'
+  conf.gem :github => 'ksss/mruby-signal', :branch => 'master'
   if RUBY_PLATFORM =~ /linux/i
     conf.gem :github => 'kjunichi/mruby-inotify'
     conf.gem :github => 'ksss/mruby-file-stat'
-    conf.gem :github => 'ksss/mruby-signal', :branch => 'master'
   end
 
   # include the default GEMs
