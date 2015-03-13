d5b8aa1e9fa84dc93188c2c3d7e84b3f413245d2
Junichi Kajiwara <junichi.kajiwara@gmail.com>
2015-03-14 06:08:27 +0900

    add boot_from_file_path_cgi

--- examples/testOnly.js
+++ examples/testOnly.js
@@ -3,8 +3,10 @@ var ref = require('ref');
 var ffi = require('ffi');
 
 var funcPtr = ffi.Function('int', ['string']);
+var funcCgiPtr = ffi.Function('string', ['string']);
 var mylib = ffi.Library('./libtrusterd', {
-  'boot_from_file_path': ['int', ['string', funcPtr]]
+  'boot_from_file_path': ['int', ['string', funcPtr]],
+  'boot_from_file_path_cgi': ['int', ['string', funcCgiPtr]]
 });
 
 // onReult will call by trusterd.
@@ -13,6 +15,10 @@ var onResult = function(resultVal) {
   return 0;
 }
 
+var onRequest = function(resultVal) {
+  console.log('Result is', resultVal);
+  return "<html>Hello, trusted,this is node.js.</html>";
+}
 // start http2 trusterd.
-mylib.boot_from_file_path("examples/test.conf.rb",onResult);
+mylib.boot_from_file_path_cgi("examples/test.conf.rb",onRequest);
 //
